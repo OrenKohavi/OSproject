cc5249f26c345fbeaadca9b50c31b9f84ded8080
Oren Kohavi <oren_kohavi@brown.edu>
2024-08-28 00:53:53 -0700

    Prints things

--- bootsector_code.s
+++ bootsector_code.s
@@ -1,13 +1,14 @@
 .code16 #16-bit real mode
 
 .globl _start
+.org=0x7c00
 
 _start:
     mov $0x7C00, %sp #Setup the stack (We have until 0x500 overwritable)
-    mov msg_welcome, %ax
+    mov $msg_welcome, %ax
     call bios_print_string #Print 'Hello, World!'
-    call delay
-    mov msg_loading_os, %ax
+    #call delay
+    mov $msg_loading_os, %ax
     call bios_print_string
     hlt
 
@@ -20,13 +21,18 @@ bios_print_string:
     mov %ax, %si #si is the pointer to the string now
     #Setup BIOS text syscall
     mov $0x0E, %ah
-    mov $0x0700, %bx
+    mov $0x0007, %bx
 _bios_print_string_loop:
     mov (%si), %al #Deref character
     inc %si #Increment string pointer
     cmp $0, %al #If null terminator reached return
+    je _bios_print_string_exit
     int $0x10 #Else null terminator not reached (do BIOS syscall)
-    jmp bios_print_string #loop again to print the next char
+    jmp _bios_print_string_loop #loop again to print the next char
+_bios_print_string_exit:
+    pop %si
+    pop %bx
+    ret
 
 # **DELAY**
 # preserves all registers
@@ -54,6 +60,6 @@ _delay_level3_loop:
     ret
 
 msg_welcome:
-    .asciz "Bootloader Loaded - Hello, World!"
+    .asciz "Bootloader Loaded - Hello, World!\r\n"
 msg_loading_os:
     .asciz "Attempting to load OS into memory..."
